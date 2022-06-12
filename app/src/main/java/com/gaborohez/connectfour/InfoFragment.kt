fbd7877a196f0c750691c6914649f2638722dda3
Gabo Rohez <gabow95k@gmail.com>
2022-06-12 00:28:52 -0500

    add design and intent for github and email

--- app/src/main/java/com/gaborohez/connectfour/InfoFragment.kt
+++ app/src/main/java/com/gaborohez/connectfour/InfoFragment.kt
@@ -1,12 +1,16 @@
 package com.gaborohez.connectfour
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.gaborohez.connectfour.databinding.FragmentInfoBinding
-import com.gaborohez.connectfour.databinding.FragmentMenuBinding
+
 
 class InfoFragment : Fragment() {
 
@@ -19,6 +23,28 @@ class InfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvVersion.text = String.format(requireActivity().getString(R.string.version), BuildConfig.VERSION_NAME)
+        setUpEvents()
+    }
+
+    private fun setUpEvents() {
+        binding.contentGithub.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(requireActivity().getString(R.string.github_url))
+            startActivity(i)
+        }
+
+        binding.contentEmail.setOnClickListener {
+
+            try {
+                val i = Intent(Intent.ACTION_SEND)
+                i.type = "message/rfc822"
+                i.putExtra(Intent.EXTRA_EMAIL, arrayOf(requireActivity().getString(R.string.email)))
+                startActivity(Intent.createChooser(i, getString(R.string.message_from_intent_email)))
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(requireActivity(), getString(R.string.no_email_client), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
